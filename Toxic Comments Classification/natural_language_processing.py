ea196c98f52a8f5d989a1d3da283a76faeb4a5f6
manishprasadthapliyal <manish54.thapliyal@gmail.com>
2018-02-27 16:56:59 +0530

    Update natural_language_processing.py

--- Toxic Comments Classification/natural_language_processing.py
+++ Toxic Comments Classification/natural_language_processing.py
@@ -5,7 +5,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 from sklearn.decomposition import PCA
-rows = 1000
+rows = 5000
 # Import dataset
 dataset = pd.read_csv('train.csv')
 # CLeaning the texts
